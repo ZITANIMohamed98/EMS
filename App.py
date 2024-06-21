28973d180796deff71d62347bf245f4d80fe0072
ZITANIMohamed98 <zitaniceo@gmail.com>
2024-06-21 22:21:12 +0200

    "Added PDF generation functionality, refactored code to create a report with a letterhead, title, and embedded image, and updated requirements.txt to include FPDF"

--- App.py
+++ App.py
@@ -2,40 +2,106 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import sys
 import json
+from fpdf import FPDF 
+import datetime
+
+def create_letterhead(pdf, WIDTH):
+    pdf.image("./resources/letterhead.png", 0, 0, WIDTH)
+
+def create_title(title, pdf):
+    
+    # Add main title
+    pdf.set_font('Helvetica', 'b', 20)  
+    pdf.ln(40)
+    pdf.write(5, title)
+    pdf.ln(10)
+    
+    # Add date of report
+    pdf.set_font('Helvetica', '', 14)
+    pdf.set_text_color(r=128,g=128,b=128)
+    today = time.strftime("%d/%m/%Y")
+    pdf.write(4, f'{today}')
+    
+    # Add line break
+    pdf.ln(10)
+
+def write_to_pdf(pdf, words):
+    
+    # Set text colour, font size, and font type
+    pdf.set_text_color(r=0,g=0,b=0)
+    pdf.set_font('Helvetica', '', 12)
+    
+    pdf.write(5, words)
+    
+def main(file):
+
+        f = open(file)
+
+        data = json.load(f)
+
+        imp_dict = data.items()
+        imp_list = list(data.items())[1][1]
+
+        mait_list = list(data.items())[0][1]
+
+        Importance = np.array(list(imp_list.values()))
+
+        Maitrise = np.array(list(mait_list.values()))
+
+        # multiplying the arrays "importance" and "maitrise" to get the final scores
+        result = np.multiply(Importance,Maitrise)
 
 
-file = sys.argv[1]
 
-f = open(file)
+        # Opening JSON file
+        #f = open('data.json')
+        # creating the dataset
+        courses = list(data["Importance"].keys())
+        values = list(result)
+        
+        fig = plt.figure(figsize = (10, 5))
+        
+        # creating the bar plot
+        plt.bar(courses, values, color ='green', 
+                width = 0.4)
+        
+        plt.xlabel("Aspects environmentaux")
+        plt.ylabel("Indice d'impact")
+        plt.title("etat de la gestion environnementale - Mois de Mai")
+        plt.savefig('./resources/rapportHebdomadaire.png')
+        
+        # Global Variables
+        TITLE = "Monthly Business Report"
+        WIDTH = 210
+        HEIGHT = 297
 
-data = json.load(f)
+        # Create PDF
+        pdf = FPDF() # A4 (210 by 297 mm)
 
-imp_dict = data.items()
-imp_list = list(data.items())[1][1]
 
-mait_list = list(data.items())[0][1]
+        '''
+        First Page of PDF
+        '''
+        # Add Page
+        pdf.add_page()
 
-Importance = np.array(list(imp_list.values()))
+        # Add lettterhead and title
+        create_letterhead(pdf, WIDTH)
+        create_title(TITLE, pdf)
 
-Maitrise = np.array(list(mait_list.values()))
+        # Add some words to PDF
+        write_to_pdf(pdf, "1. The table below illustrates the annual sales of Heicoders Academy:")
+        pdf.ln(15)
 
-# multiplying the arrays "importance" and "maitrise" to get the final scores
-result = np.multiply(Importance,Maitrise)
+        
 
-print(result)
-# Opening JSON file
-#f = open('data.json')
-# creating the dataset
-courses = list(data["Importance"].keys())
-values = list(result)
-  
-fig = plt.figure(figsize = (10, 5))
- 
-# creating the bar plot
-plt.bar(courses, values, color ='green', 
-        width = 0.4)
- 
-plt.xlabel("Aspects environmentaux")
-plt.ylabel("Indice d'impact")
-plt.title("Students enrolled in different courses")
-plt.savefig('test.png')
+        # Add the generated visualisations to the PDF
+        pdf.image("resources/rapportHebdomadaire.png", 5, 200, WIDTH*4)
+        pdf.ln(10)
+        
+
+        # Add some words to PDF
+        write_to_pdf(pdf, "2. The visualisations below shows the trend of total sales for Heicoders Academy and the breakdown of revenue for year 2016:")
+
+        # Generate the PDF
+        pdf.output("rapport_hebdomadaire"+str(datetime.datetime.now())+".pdf", 'F')
--- window.py
+++ window.py
@@ -1,6 +1,9 @@
 from tkinter import ttk 
 from tkinter import * 
 import tkinter as tk
+from App import main
+
+file = "data.json"
 # Top level window 
 root = Tk()
 frame = ttk.Frame(root, padding=10)
@@ -13,21 +16,24 @@ root.geometry('400x200')
 # from textbox and printing it  
 # at label widget 
   
-def printInput(): 
+def saveInput(): 
     inp = inputtxt1.get(1.0, "end-1c") 
-    lbl.config(text = "Provided Input: "+inp) 
+    
 
-desc1 = tk.Label(frame, text="Hello World!")
+message = tk.Label(frame, text="Veuillez Introduire les indices d'impact ")
+Implbl = tk.Label(frame, text="Importance")
+
+ImpEaudesc = tk.Label(frame, text="Eau")
 
 # TextBox Creation 
-inputtxt1 = tk.Text(frame, 
+ImpEau = tk.Text(frame, 
                    height = 1, 
                    width = 5) 
 
-desc1.grid(column=0, row=0)   # grid dynamically divides the space in a grid
-inputtxt1.grid(column=1, row=0)
+ImpEaudesc.grid(column=0, row=0)   # grid dynamically divides the space in a grid
+ImpEau.grid(column=1, row=0)
  
-desc2 = tk.Label(frame, text="Hello World!")
+desc2 = tk.Label(frame, text="Bruit")
 
 # TextBox Creation 
 inputtxt2 = tk.Text(frame, 
@@ -37,7 +43,7 @@ inputtxt2 = tk.Text(frame,
 desc2.grid(column=0, row=1)   # grid dynamically divides the space in a grid
 inputtxt2.grid(column=1, row=1)
 
-desc2 = tk.Label(frame, text="Hello World!")
+desc2 = tk.Label(frame, text="Air")
 
 # TextBox Creation 
 inputtxt2 = tk.Text(frame, 
@@ -46,7 +52,7 @@ inputtxt2 = tk.Text(frame,
   
 desc2.grid(column=0, row=2)   # grid dynamically divides the space in a grid
 inputtxt2.grid(column=1, row=2)
-desc2 = tk.Label(frame, text="Hello World!")
+desc2 = tk.Label(frame, text="Energie")
 
 # TextBox Creation 
 inputtxt2 = tk.Text(frame, 
@@ -55,7 +61,57 @@ inputtxt2 = tk.Text(frame,
   
 desc2.grid(column=0, row=3)   # grid dynamically divides the space in a grid
 inputtxt2.grid(column=1, row=3)
-desc2 = tk.Label(frame, text="Hello World!")
+desc2 = tk.Label(frame, text="Dechets")
+
+# TextBox Creation 
+inputtxt2 = tk.Text(frame, 
+                   height = 1, 
+                   width = 5) 
+  
+desc2.grid(column=0, row=4)   # grid dynamically divides the space in a grid
+inputtxt2.grid(column=1, row=4)
+
+Implbl = tk.Label(frame, text="\Maitrise")
+
+ImpEaudesc = tk.Label(frame, text="Eau")
+
+# TextBox Creation 
+ImpEau = tk.Text(frame, 
+                   height = 1, 
+                   width = 5) 
+
+ImpEaudesc.grid(column=0, row=0)   # grid dynamically divides the space in a grid
+ImpEau.grid(column=1, row=0)
+ 
+desc2 = tk.Label(frame, text="Bruit")
+
+# TextBox Creation 
+inputtxt2 = tk.Text(frame, 
+                   height = 1, 
+                   width = 5) 
+  
+desc2.grid(column=0, row=1)   # grid dynamically divides the space in a grid
+inputtxt2.grid(column=1, row=1)
+
+desc2 = tk.Label(frame, text="Air")
+
+# TextBox Creation 
+inputtxt2 = tk.Text(frame, 
+                   height = 1, 
+                   width = 5) 
+  
+desc2.grid(column=0, row=2)   # grid dynamically divides the space in a grid
+inputtxt2.grid(column=1, row=2)
+desc2 = tk.Label(frame, text="Energie")
+
+# TextBox Creation 
+inputtxt2 = tk.Text(frame, 
+                   height = 1, 
+                   width = 5) 
+  
+desc2.grid(column=0, row=3)   # grid dynamically divides the space in a grid
+inputtxt2.grid(column=1, row=3)
+desc2 = tk.Label(frame, text="Dechets")
 
 # TextBox Creation 
 inputtxt2 = tk.Text(frame, 
@@ -65,11 +121,13 @@ inputtxt2 = tk.Text(frame,
 desc2.grid(column=0, row=4)   # grid dynamically divides the space in a grid
 inputtxt2.grid(column=1, row=4)
 # Button Creation 
-printButton = tk.Button(frame, 
-                        text = "Print",  
-                        command = printInput) 
+saveButton = tk.Button(frame, 
+                        text = "Save",  
+                        command = SaveInput) 
 
-
+GenerateReport = tk.Button(frame, 
+                        text = "Generer le rapport",  
+                        command = main(file))
 # Label Creation 
 lbl = tk.Label(frame, text = "") 
 
